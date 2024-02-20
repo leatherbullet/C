0c83f5715773baa1464d0463f8b4a184daa8e48f
79612 <79612@DESKTOP-O1P5TC5>
2024-02-20 17:55:47 +0300

    Добавьте файлы проекта.

---
+++ C/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int visitors;
+            int waitingTimeVisitorInMinuts = 10;
+            int waitingTimeInMinuts;
+            int minutInHour;
+            int hourInTurn;
+            int minutsInTurn;
+
+            Console.WriteLine($"время приема посетителя:{waitingTimeVisitorInMinuts} минут");
+            Console.Write("Сколько человек в очереди?");
+            visitors = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("сколько минут в одном часе?");
+            minutInHour = Convert.ToInt32(Console.ReadLine());
+
+            waitingTimeInMinuts = visitors * waitingTimeVisitorInMinuts;
+            hourInTurn = waitingTimeInMinuts / minutInHour;
+            minutsInTurn = waitingTimeInMinuts % minutInHour;
+
+            Console.WriteLine($"время ожидания в очереди {hourInTurn} часов и {minutsInTurn} минут");
+        }
+    }
+}
